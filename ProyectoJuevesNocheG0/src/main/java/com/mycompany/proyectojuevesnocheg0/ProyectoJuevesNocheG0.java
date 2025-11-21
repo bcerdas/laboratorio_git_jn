35371a4275b4befa5a5c24b0846ce9e4a5a18f94
bcerdas <bryan.cerdas07@gmail.com>
2025-11-20 19:51:12 -0600

    first commit

---
+++ ProyectoJuevesNocheG0/src/main/java/com/mycompany/proyectojuevesnocheg0/ProyectoJuevesNocheG0.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectojuevesnocheg0;
+
+/**
+ *
+ * @author bryan
+ */
+public class ProyectoJuevesNocheG0 {
+
+    public static void main(String[] args) {
+        System.out.println("Este es mi proyecto");
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# laboratorio_git_jn" 
